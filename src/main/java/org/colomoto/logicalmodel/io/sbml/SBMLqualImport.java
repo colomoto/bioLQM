14be1338e94adab20caa3eaf0ee6ddc3f3d78740
Aurélien Naldi <aurelien.naldi@gmail.com>
2012-09-19 15:16:17 +0200

    MOD: handle EQ, NEQ, GEQ, LEQ, LT, and GT relations in SBML import instead of only EQ

--- src/main/java/org/colomoto/logicalmodel/io/sbml/SBMLqualImport.java
+++ src/main/java/org/colomoto/logicalmodel/io/sbml/SBMLqualImport.java
@@ -186,67 +186,7 @@ public class SBMLqualImport {
 		case RELATIONAL_LT:
 		case RELATIONAL_NEQ:
 		case RELATIONAL_EQ:
-			if (math.getChildCount() != 2) {
-				throw new RuntimeException("Invalid relation: "+math);
-			}
-
-			ASTNode varNode = math.getChild(0);
-			ASTNode valueNode = math.getChild(1);
-			
-			if (varNode.getType() != Type.NAME) {
-				if (valueNode.getType() == Type.NAME) {
-					varNode = valueNode;
-					valueNode = math.getChild(0);
-				} else {
-					throw new RuntimeException("Invalid relation children: "+math);
-				}
-			}
-			
-			Type valueType = valueNode.getType();
-			int relValue = -1;
-			if (valueType == Type.INTEGER) {
-				relValue = valueNode.getInteger();
-			} else if (valueType == Type.NAME) {
-				// TODO: support named values
-				throw new RuntimeException("Constant relation values not yet supported: "+valueNode);
-			} else {
-				throw new RuntimeException("Unrecognised relation value: "+valueNode);
-			}
-			
-			name = varNode.getName();
-			index = getIndexForName(name);
-			var = ddvariables[index];
-
-			if (0 > relValue || var.nbval <= relValue) {
-				throw new RuntimeException("Relation value out of [0.."+var.nbval+"[ range: "+valueNode);
-			}
-
-			
-			switch (type) {
-			
-			case RELATIONAL_GEQ:
-			case RELATIONAL_GT:
-			case RELATIONAL_LEQ:
-			case RELATIONAL_LT:
-			case RELATIONAL_NEQ:
-				// TODO: support more relations
-				throw new RuntimeException("non-equal relations not yet supported: "+math);
-				
-			case RELATIONAL_EQ:
-				if (var.nbval == 2) {
-					if (relValue == 0) {
-						return var.getNode(value, 0);
-					}
-					return var.getNode(0, value);
-				}
-				
-				int[] values = new int[var.nbval];
-				values[relValue] = value;
-				return var.getNode(values);
-
-			default:
-				throw new RuntimeException("unknown relation type: "+math);
-			}
+			return getMDDForRelation(math, value);
 			
 		case CONSTANT_FALSE:
 			return 0;
@@ -300,6 +240,189 @@ public class SBMLqualImport {
 		}
 	}
 	
+	/**
+	 * Parse a relation term and get a matching MDD.
+	 * 
+	 * @param relation an ASTNode corresponding to a relation
+	 * @param value the value to return when the relation is satisfied
+	 * @return a MDD testing this relation
+	 */
+	private int getMDDForRelation(ASTNode relation, int value) {
+		
+		Type type = relation.getType();
+		
+		// consistency check: should only be called for relation nodes
+		switch (type) {
+		case RELATIONAL_GEQ:
+		case RELATIONAL_GT:
+		case RELATIONAL_LEQ:
+		case RELATIONAL_LT:
+		case RELATIONAL_NEQ:
+		case RELATIONAL_EQ:
+			break;
+		default:
+			throw new RuntimeException("Not a relation: "+relation);
+		}
+		
+		// a relation should always have two children
+		if (relation.getChildCount() != 2) {
+			throw new RuntimeException("Invalid relation: "+relation);
+		}
+		ASTNode varNode = relation.getChild(0);
+		ASTNode valueNode = relation.getChild(1);
+
+		
+		// try to detect reversed relations ( "1 == g2" instead of "g2 == 1" )
+		if (valueNode.getType() == Type.NAME && varNode.getType() == Type.INTEGER) {
+			varNode = valueNode;
+			valueNode = relation.getChild(0);
+
+			// reverse inequalities as well
+			switch (type) {
+			case RELATIONAL_GEQ:
+				type = Type.RELATIONAL_LEQ;
+				break;
+			case RELATIONAL_LEQ:
+				type = Type.RELATIONAL_GEQ;
+				break;
+			case RELATIONAL_GT:
+				type = Type.RELATIONAL_LT;
+				break;
+			case RELATIONAL_LT:
+				type = Type.RELATIONAL_GT;
+				break;
+			}
+		}
+
+		
+		// extract the variable and value from the children 
+		// TODO: support named values
+		if (varNode.getType() != Type.NAME || valueNode.getType() != Type.INTEGER) {
+			throw new RuntimeException("Missing name or unsupported value in relation: "+relation);
+		}
+		
+		String name = varNode.getName();
+		int index = getIndexForName(name);
+		if (index < 0) {
+			throw new RuntimeException("Unrecognized name in relation: "+relation);
+		}
+
+		MDDVariable var = ddvariables[index];
+		int relValue = valueNode.getInteger();
+		
+		
+		// handle border cases, that are always true or false
+		switch (type) {
+		
+		case RELATIONAL_GT:
+			type = Type.RELATIONAL_GEQ;
+			relValue += 1;
+		case RELATIONAL_GEQ:
+			if (relValue <= 0) {
+				return value;
+			}
+			if (relValue >= var.nbval) {
+				return 0;
+			}
+			break;
+
+			
+		case RELATIONAL_LEQ:
+			type = Type.RELATIONAL_LT;
+			relValue += 1;
+		case RELATIONAL_LT:
+			if (relValue >= var.nbval) {
+				return value;
+			}
+			if (relValue <= 0) {
+				return 0;
+			}
+			break;
+
+			
+		case RELATIONAL_NEQ:
+			if (relValue < 0 || relValue >= var.nbval) {
+				return value;
+			}
+			break;
+			
+		case RELATIONAL_EQ:
+			if (relValue < 0 || relValue >= var.nbval) {
+				return 0;
+			}
+			break;
+			
+		default:
+			throw new RuntimeException("unknown relation type: "+relation);
+		}
+
+		
+		
+		// now we should have a valid relValue and only EQ, NEQ, GEQ or LT relations
+		if (0 > relValue || var.nbval <= relValue) {
+			throw new RuntimeException("Relation value out of [0.."+var.nbval+"[ range: "+valueNode);
+		}
+
+		
+		if (var.nbval == 2) {
+			switch (type) {
+			
+			case RELATIONAL_LT:
+				return var.getNode(value, 0);
+				
+			case RELATIONAL_GEQ:
+				return var.getNode(0, value);
+				
+			case RELATIONAL_EQ:
+				if (relValue == 0) {
+					return var.getNode(value, 0);
+				}
+				return var.getNode(0, value);
+
+			case RELATIONAL_NEQ:
+				if (relValue == 0) {
+					return var.getNode(0, value);
+				}
+				return var.getNode(value, 0);
+			}
+			
+			throw new RuntimeException("Could not handle relation: "+relation);
+		}
+
+		
+		int[] values = new int[var.nbval];
+		switch (type) {
+		
+		case RELATIONAL_GEQ:
+			for (int v=relValue ; v<var.nbval ; v++) {
+				values[v] = value;
+			}
+			return var.getNode(values);
+		
+		case RELATIONAL_LT:
+			for (int v=0 ; v<relValue ; v++) {
+				values[v] = value;
+			}
+			return var.getNode(values);
+
+		case RELATIONAL_NEQ:
+			for (int v=0 ; v<var.nbval ; v++) {
+				if (v == relValue) {
+					values[v] = 0;
+				} else {
+					values[v] = value;
+				}
+			}
+			return var.getNode(values);
+			
+		case RELATIONAL_EQ:
+			values[relValue] = value;
+			return var.getNode(values);
+
+		}
+		
+		throw new RuntimeException("Could not handle relation: "+relation);
+	}
 	
 	
 	public static void main(String[] args) {
