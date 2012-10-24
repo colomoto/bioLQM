745b4d81ce49be2d845d8015e9ec342d4ca96537
Pedro Tiago Monteiro <ptgm@igc.gulbenkian.pt>
2012-10-24 09:54:30 +0100

    ADD: Support to export logical models into the old (non xml) GNA format.

---
+++ src/main/java/org/colomoto/logicalmodel/io/gna/GNAFormat.java
@@ -0,0 +1,30 @@
+package org.colomoto.logicalmodel.io.gna;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.colomoto.logicalmodel.LogicalModel;
+import org.colomoto.logicalmodel.io.AbstractFormat;
+import org.colomoto.logicalmodel.io.LogicalModelFormat;
+import org.mangosdk.spi.ProviderFor;
+
+/**
+ * Format description for GNA files.
+ * These files are used by the Genetic Network Analyzer (GNA) tool.
+ * 
+ * @author Pedro T. Monteiro
+ */
+@ProviderFor(LogicalModelFormat.class)
+public class GNAFormat extends AbstractFormat {
+
+	public GNAFormat() {
+		super("gna", "GNA format", true, false);
+	}
+	
+	@Override
+	public void export(LogicalModel model, OutputStream out) throws IOException {
+		GNAExport gna = new GNAExport();
+		gna.export(model, out);
+	}
+
+}
