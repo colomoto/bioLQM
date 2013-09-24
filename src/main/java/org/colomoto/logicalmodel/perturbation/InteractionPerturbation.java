aca82787a8ebdcc1324c4522faede513592e4d9e
Aurélien Naldi <aurelien.naldi@gmail.com>
2013-09-24 13:48:03 +0200

    NEW: add perturbation to remove a regulator

---
+++ src/main/java/org/colomoto/logicalmodel/perturbation/InteractionPerturbation.java
@@ -0,0 +1,66 @@
+package org.colomoto.logicalmodel.perturbation;
+
+import org.colomoto.logicalmodel.LogicalModel;
+import org.colomoto.logicalmodel.NodeInfo;
+import org.colomoto.mddlib.MDDManager;
+import org.colomoto.mddlib.MDDVariable;
+
+/**
+ * A perturbation which removes the effect of a component on another (i.e. removes an interaction).
+ * The target component will behave as if this regulator had a fixed value.
+ * 
+ * @author Aurelien Naldi
+ */
+public class InteractionPerturbation extends AbstractPerturbation {
+
+	public final int regValue;
+	public final NodeInfo target;
+	public final NodeInfo regulator;
+
+	public InteractionPerturbation(NodeInfo regulator, NodeInfo target, int regValue) {
+		this.regValue = regValue;
+		this.target = target;
+		this.regulator = regulator;
+	}
+	
+	@Override
+	public void update(LogicalModel model) {
+		int idx = -1;
+		int[] functions = null;
+		
+		idx = model.getNodeOrder().indexOf(target);
+		if (idx >= 0) {
+			functions = model.getLogicalFunctions();
+		} else {
+			idx = model.getExtraComponents().indexOf(target);
+			if (idx >= 0) {
+				functions = model.getExtraLogicalFunctions();
+			}
+		}
+		
+		if (idx < 0) {
+			throw new RuntimeException("Perturbation.update(): Could not find the target component");
+		}
+		
+		int oldValue = functions[idx];
+		MDDManager manager = model.getMDDManager();
+		MDDVariable var = manager.getVariableForKey(regulator);
+		RegulatorRemovalOperation op = new RegulatorRemovalOperation(model.getMDDManager(), var, regValue);
+		functions[idx] = op.restrict(oldValue);
+		
+		model.getMDDManager().free(oldValue);
+	}
+
+	public String toString() {
+		return target.getNodeID() + " ["+ regulator.getNodeID() + "@"+regValue+"]";
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof InteractionPerturbation) {
+			InteractionPerturbation p = (InteractionPerturbation)o;
+			return p.regulator.equals(this.regulator) && p.target.equals(this.target) && p.regValue == this.regValue; 
+		}
+		return false;
+	}
+
+}
