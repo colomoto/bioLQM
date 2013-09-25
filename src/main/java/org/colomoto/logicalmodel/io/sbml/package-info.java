cc8f47f289bafcbabd89de4bf1a446026c906917
Aurélien Naldi <aurelien.naldi@gmail.com>
2013-09-25 16:02:55 +0200

    CLEAN: some package-level javadoc

--- src/main/java/org/colomoto/logicalmodel/io/sbml/package-info.java
+++ src/main/java/org/colomoto/logicalmodel/io/sbml/package-info.java
@@ -1,5 +1,5 @@
 /**
- * SBML support.
- * This packages uses JSBML to export and import SBML files using the "qual" extension.
+ * SBML qual support.
+ * This packages uses JSBML to export and import SBML files using the SBML "qual" extension.
  */
 package org.colomoto.logicalmodel.io.sbml;
