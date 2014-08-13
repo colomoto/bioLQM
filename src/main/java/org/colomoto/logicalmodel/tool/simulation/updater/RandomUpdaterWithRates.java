be08fdc351b972be00ba7b8b311fa37e90fb3ea3
Pedro Tiago Monteiro <ptgm@kdbio.inesc-id.pt>
2014-08-13 09:00:04 +0100

    moved weights to rates + WeightedRandomUpdater.java -> RandomUpdaterWithRates.java

--- src/main/java/org/colomoto/logicalmodel/tool/simulation/updater/RandomUpdaterWithRates.java
+++ src/main/java/org/colomoto/logicalmodel/tool/simulation/updater/RandomUpdaterWithRates.java
@@ -3,17 +3,17 @@ package org.colomoto.logicalmodel.tool.simulation.updater;
 import org.colomoto.logicalmodel.LogicalModel;
 
 /**
- * Updater picking a random successor in the asynchronous scheme, with optional weight.
+ * Updater picking a random successor in the asynchronous scheme, with optional rates.
  *
  * @author Aurelien Naldi
  */
-public class WeightedRandomUpdater extends AbstractRandomUpdater {
+public class RandomUpdaterWithRates extends AbstractRandomUpdater {
 
-    // the weights associated to each component
-    private final int[] weights;
+    // the rates associated to each component
+    private final int[] rates;
 
-    // the changes and weights which are active for the current step
-    private final int[] step_weights;
+    // the changes and rates which are active for the current step
+    private final int[] step_rates;
     private final int[][] step_changes;
 
 	/**
@@ -21,31 +21,31 @@ public class WeightedRandomUpdater extends AbstractRandomUpdater {
 	 *
 	 * @param model
 	 */
-    public WeightedRandomUpdater(LogicalModel model) {
+    public RandomUpdaterWithRates(LogicalModel model) {
         this(model, null);
     }
 
-    public WeightedRandomUpdater(LogicalModel model, int[] weights) {
+    public RandomUpdaterWithRates(LogicalModel model, int[] rates) {
         super(model);
-        if (weights == null) {
-            this.weights = new int[this.size];
-            for (int i=0 ; i<this.weights.length ; i++) {
-                this.weights[i] = 1;
+        if (rates == null) {
+            this.rates = new int[this.size];
+            for (int i=0 ; i<this.rates.length ; i++) {
+                this.rates[i] = 1;
             }
         } else {
-            this.weights = weights;
+            this.rates = rates;
         }
 
         // create empty arrays to store the available changes at each steps
         // reusing these arrays means that this class is NOT thread safe
-        this.step_weights = new int[this.size];
+        this.step_rates = new int[this.size];
         this.step_changes = new int[this.size][2];
     }
 
 	@Override
 	public byte[] pickSuccessor(byte[] state) {
         int nb_changes = 0;
-        int totalweight = 0;
+        int totalrate = 0;
 
 		for (int idx=0 ; idx < size ; idx++) {
 			int change = nodeChange(state, idx);
@@ -54,9 +54,9 @@ public class WeightedRandomUpdater extends AbstractRandomUpdater {
             }
 
             // store the available change
-            int w = this.weights[idx];
-            totalweight += w;
-            step_weights[nb_changes] = totalweight;
+            int r = this.rates[idx];
+            totalrate += r;
+            step_rates[nb_changes] = totalrate;
             step_changes[nb_changes][0] = idx;
             step_changes[nb_changes][1] = change;
             nb_changes++;
@@ -67,29 +67,29 @@ public class WeightedRandomUpdater extends AbstractRandomUpdater {
         }
 
         byte[] nextstate = state.clone();
-        int[] selected = select(nb_changes, totalweight);
+        int[] selected = select(nb_changes, totalrate);
         nextstate[ selected[0] ] += selected[1];
 		return nextstate;
 	}
 
     /**
-     * Select one of the weighted changes randomly.
+     * Select one of the rated changes randomly.
      *
-     * It will pick the largest position such that the sum of previous active weights is larger than a random threshold.
+     * It will pick the largest position such that the sum of previous active rates is larger than a random threshold.
      *
-     * @param totalweight
+     * @param totalrate
      * @return
      */
-    private int[] select(int nb_changes, int totalweight) {
+    private int[] select(int nb_changes, int totalrate) {
         if (nb_changes == 1) {
             return step_changes[0];
         }
 
         int s = 0;
         int idx = 0;
-        int r = random.nextInt(totalweight);
+        int r = random.nextInt(totalrate);
         for (  ; idx<nb_changes ; idx++) {
-            s += this.step_weights[idx];
+            s += this.step_rates[idx];
             if (s > r) {
                 break;
             }
