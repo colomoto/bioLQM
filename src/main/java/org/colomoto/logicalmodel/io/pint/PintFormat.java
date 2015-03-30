4c2bd6aeb4c017bddfb308b4e195b34b3eed9e34
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2015-03-30 22:13:36 +0200

    add export to Pint format

---
+++ src/main/java/org/colomoto/logicalmodel/io/pint/PintFormat.java
@@ -0,0 +1,32 @@
+package org.colomoto.logicalmodel.io.pint;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.colomoto.logicalmodel.LogicalModel;
+import org.colomoto.logicalmodel.io.AbstractFormat;
+import org.colomoto.logicalmodel.io.LogicalModelFormat;
+import org.mangosdk.spi.ProviderFor;
+
+@ProviderFor(LogicalModelFormat.class)
+public class PintFormat extends AbstractFormat {
+
+	public PintFormat() { super("an", "Pint format", true); }
+
+	/*
+	@Override
+	public LogicalModel importFile(File f) throws IOException {
+		PintImport importer = new PintImport();
+		importer.parse(f);
+		return importer.getModel();
+	}
+	*/
+
+	@Override
+	public void export(LogicalModel model, OutputStream out) throws IOException {
+		PintExport exporter = new PintExport();
+		exporter.export(model, out);
+	}
+}
+
