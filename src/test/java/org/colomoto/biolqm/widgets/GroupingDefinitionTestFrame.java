823bc9cf2d9bb717184b7fbc304b90b429a100b1
Aurelien Naldi <aurelien.naldi@gmail.com>
2018-11-07 17:45:33 +0100

    Add a test frame for the grouping definition widget

---
+++ src/test/java/org/colomoto/biolqm/widgets/GroupingDefinitionTestFrame.java
@@ -0,0 +1,50 @@
+package org.colomoto.biolqm.widgets;
+
+import org.colomoto.biolqm.LogicalModel;
+import org.colomoto.biolqm.ReferenceModels;
+import org.colomoto.biolqm.service.LQMServiceManager;
+import org.colomoto.biolqm.tool.simulation.multiplesuccessor.ModelPriorityClasses;
+import org.colomoto.biolqm.widgets.PriorityClassPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GroupingDefinitionTestFrame {
+
+    public static void main(String[] args) throws Exception {
+
+        LogicalModel model = ReferenceModels.getModel(ReferenceModels.getNames()[0]);
+        ModelPriorityClasses mpc = new ModelPriorityClasses(model);
+
+        PriorityClassPanel pcp = new PriorityClassPanel(mpc, true);
+        pcp.updatePriorityList();
+
+        JButton b = new JButton("Print");
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println(mpc);
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.setLayout( new GridBagLayout() );
+
+        GridBagConstraints cst = new GridBagConstraints();
+        cst.weighty = cst.weightx = 1;
+        cst.fill = GridBagConstraints.BOTH;
+        panel.add(pcp, cst);
+
+        cst = new GridBagConstraints();
+        cst.gridy++;
+        panel.add(b, cst);
+
+        JFrame f = new JFrame("Test Grouping");
+        f.setSize(500,300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setContentPane(panel);
+        f.show();
+    }
+}
