5e374c48965f7ff8fa751762d5f2c6d29fd4f299
Aurelien Naldi <aurelien.naldi@gmail.com>
2018-11-30 14:36:10 +0100

    Add an iterator to expand a pattern into a stream of states

---
+++ src/main/java/org/colomoto/biolqm/helper/state/PatternStateIterator.java
@@ -0,0 +1,97 @@
+package org.colomoto.biolqm.helper.state;
+
+import java.util.Iterator;
+
+/**
+ * Iterator over all states covered by a state pattern.
+ * This iterator expands a pattern into individual states, it supports multi-valued components.
+ * All generated states use the same memory slot, users of this iterator should clone them as needed.
+ *
+ * @author Aurelien Naldi
+ */
+public class PatternStateIterator implements Iterator<byte[]> {
+
+    private final byte[] maxs;
+
+    private byte[] next = null;
+    private int[] jokers = null;
+    private int nextJoker;
+
+    /**
+     * Create an iterator over all states in a Boolean pattern.
+     *
+     * @param pattern the pattern to expand. jokers are represented by negative values.
+     */
+    public PatternStateIterator(byte[] pattern) {
+        this.maxs = new byte[pattern.length];
+        for (int i=0 ; i<maxs.length ; i++) {
+            maxs[i] = 1;
+        }
+        reset(pattern);
+    }
+
+    /**
+     * Create an iterator over all states in a multi-valued pattern.
+     *
+     * @param pattern the pattern to expand. Jokers are represented by negative values.
+     * @param maxs the max value of all components in the pattern
+     */
+    public PatternStateIterator(byte[] pattern, byte[] maxs) {
+        this.maxs = maxs;
+        reset(pattern);
+    }
+
+    /**
+     * Assign a new pattern to this iterator and start over.
+     *
+     * @param pattern the new pattern to expand. Jokers are represented by negative values.
+     */
+    public void reset(byte[] pattern) {
+        // Prepare the iterator: collect and reset joker positions
+        this.next = pattern.clone();
+        int nb_jokers = 0;
+        for (byte v: pattern) {
+            if (v < 0) {
+                nb_jokers++;
+            }
+        }
+
+        jokers = new int[nb_jokers];
+        int idx_joker = 0;
+        for (int idx=0 ; idx_joker<nb_jokers ; idx++) {
+            if (pattern[idx] < 0) {
+                jokers[idx_joker++] = idx;
+                next[idx] = 0;
+            }
+        }
+        nextJoker = 0;
+    }
+
+    private void buildNext() {
+        // Find the first joker position which can be increased
+        for (int j: jokers) {
+            if (next[j] < maxs[j]) {
+                next[j]++;
+                return;
+            }
+            // reset all previous positions
+            next[j] = 0;
+        }
+
+        // All jokers are exhausted: stop here
+        next = null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    @Override
+    public byte[] next() {
+        byte[] ret = next.clone();
+        buildNext();
+        return ret;
+    }
+
+}
--- src/main/java/org/colomoto/biolqm/io/implicanttables/ImplicantTableExport.java
+++ src/main/java/org/colomoto/biolqm/io/implicanttables/ImplicantTableExport.java
@@ -3,6 +3,7 @@ package org.colomoto.biolqm.io.implicanttables;
 import org.colomoto.biolqm.LogicalModel;
 import org.colomoto.biolqm.NodeInfo;
 import org.colomoto.biolqm.helper.implicants.RestrictedPathSearcher;
+import org.colomoto.biolqm.helper.state.PatternStateIterator;
 import org.colomoto.biolqm.io.BaseExporter;
 import org.colomoto.mddlib.MDDManager;
 
@@ -19,8 +20,14 @@ import java.util.List;
  */
 public class ImplicantTableExport extends BaseExporter {
 
+    private boolean expand = true;
+
     public ImplicantTableExport(LogicalModel model) {
         super(model);
+    }
+
+    public void expand(boolean expand) {
+        this.expand = expand;
     }
 
     public void export() throws IOException {
@@ -39,9 +46,11 @@ public class ImplicantTableExport extends BaseExporter {
             NodeInfo ni = components.get(i);
             byte[] implicant = implicants.setNode(functions[i]);
             int[] regulators = implicants.getRegulatorList();
+            byte[] maxs = new byte[regulators.length];
 
             // Header line: the list of regulators and the target component
             for (int j=0; j<regulators.length; j++) {
+                maxs[j] = components.get(regulators[j]).getMax();
                 writer.write(components.get(regulators[j]).getNodeID());
                 writer.write(" ");
             }
@@ -54,30 +63,46 @@ public class ImplicantTableExport extends BaseExporter {
             writer.write("\n");
 
             // Write all true paths from the dd
-            int len = regulators.length + 3;
+            PatternStateIterator expander = null;
+            if (expand) {
+                expander = new PatternStateIterator(maxs, maxs);
+            }
             for (int v: implicants) {
                 if (v == 0) {
                     continue;
                 }
-                char[] row = new char[len];
-                int k=0;
-                for ( int value: implicant) {
-                    // -1 stands for don't care
-                    if (value < 0) {
-                        row[k++] = '-';
-                    } else {
-                        row[k++] = (char)('0'+value);
-                    }
+                if (expander == null) {
+                    writeState(writer, implicant, v);
+                    continue;
                 }
-                row[k++] = ':';
-                row[k++] = (char)('0'+v);
-                row[k++] = '\n';
-                writer.write(row);
+
+                // expand each implicant
+                expander.reset(implicant);
+                while (expander.hasNext()) {
+                    writeState(writer, expander.next(), v);
+                }
             }
             writer.write("\n");
+
         }
 
         writer.close();
     }
 
+    private void writeState(Writer writer, byte[] state, int target) throws IOException {
+        char[] row = new char[state.length + 3];
+        int k = 0;
+        for (int value : state) {
+            // -1 stands for don't care
+            if (value < 0) {
+                row[k++] = '-';
+            } else {
+                row[k++] = (char) ('0' + value);
+            }
+        }
+        row[k++] = ':';
+        row[k++] = (char) ('0' + target);
+        row[k++] = '\n';
+        writer.write(row);
+    }
 }
