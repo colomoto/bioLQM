5a278dfb8b74399e799a54f42f1f4d2052869444
Laure <laure.de.chancel@ens.fr>
2022-06-16 14:17:53 +0200

    nouveau modifier vide

--- src/main/java/org/colomoto/biolqm/modifier/most_permissive/MostPermissiveModifier.java
+++ src/main/java/org/colomoto/biolqm/modifier/most_permissive/MostPermissiveModifier.java
@@ -1,13 +1,11 @@
-import org.colomoto.biolqm.ConnectivityMatrix;
+package org.colomoto.biolqm.modifier.most_permissive;
+
 import org.colomoto.biolqm.LogicalModel;
-import org.colomoto.biolqm.LogicalModelImpl;
 import org.colomoto.biolqm.NodeInfo;
 import org.colomoto.biolqm.modifier.BaseModifier;
 import org.colomoto.mddlib.IndexMapper;
 import org.colomoto.mddlib.MDDManager;
 import org.colomoto.mddlib.MDDMapper;
-import org.colomoto.mddlib.MDDVariable;
-import org.colomoto.mddlib.internal.MDDStoreImpl;
 
 import java.util.*;
 
@@ -15,13 +13,7 @@ import java.util.*;
  *
  * @author
  */
-public class MostPermissiveModifier extends BaseModifier implements IndexMapper {
-
-    private final LogicalModel model;
-    private MDDManager ddm, newDDM;
-    private List<NodeInfo> core, extra, newCore, newExtra;
-    private int[] coreFunctions, extraFunctions, newCoreFunctions, newExtraFunctions;
-
-    }
+public abstract class MostPermissiveModifier extends BaseModifier implements IndexMapper {
+}
 
 
--- src/main/java/org/colomoto/biolqm/modifier/most_permissive/MostPermissiveService.java
+++ src/main/java/org/colomoto/biolqm/modifier/most_permissive/MostPermissiveService.java
@@ -1,13 +1,27 @@
+package org.colomoto.biolqm.modifier.most_permissive;
 
 import org.colomoto.biolqm.LogicalModel;
 import org.colomoto.biolqm.modifier.ModelModifier;
 import org.colomoto.biolqm.modifier.ModelModifierService;
+import org.colomoto.biolqm.modifier.buffer.BufferingModifier;
 import org.colomoto.biolqm.service.BaseService;
 import org.colomoto.biolqm.service.MultivaluedSupport;
 import org.kohsuke.MetaInfServices;
 
 @MetaInfServices(ModelModifierService.class)
 
-public class MostPermissive extends BaseService implements ModelModifierService {
+public abstract class MostPermissiveService extends BaseService implements ModelModifierService {
+    /** The identifier used to retrieve this service by name */
+    public static final String ID = "buffer";
+    private static final String NAME = "add buffer components to a model";
+    private static final String DESCR = ":buffer | :delay | source:target1,target2";
+
+    /**
+     * Public constructor which should only be used for service discovery.
+     */
+    public MostPermissiveService() {
+        super(ID, NAME, DESCR, MultivaluedSupport.MULTIVALUED);
+    }
+
 
 }
