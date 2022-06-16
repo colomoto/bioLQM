c5964c634d9e5c038ff28015c8f1c5aa098ccc9f
Laure <laure.de.chancel@ens.fr>
2022-06-16 14:33:32 +0200

    structure

--- src/main/java/org/colomoto/biolqm/modifier/most_permissive/MostPermissiveModifier.java
+++ src/main/java/org/colomoto/biolqm/modifier/most_permissive/MostPermissiveModifier.java
@@ -1,5 +1,6 @@
 package org.colomoto.biolqm.modifier.most_permissive;
 
+import org.colomoto.biolqm.ConnectivityMatrix;
 import org.colomoto.biolqm.LogicalModel;
 import org.colomoto.biolqm.NodeInfo;
 import org.colomoto.biolqm.modifier.BaseModifier;
@@ -13,7 +14,35 @@ import java.util.*;
  *
  * @author
  */
-public abstract class MostPermissiveModifier extends BaseModifier implements IndexMapper {
+public class MostPermissiveModifier extends BaseModifier implements IndexMapper {
+    private final LogicalModel model;
+    private MDDManager ddm, newDDM;
+    private List<NodeInfo> core, extra, newCore, newExtra;
+    private int[] coreFunctions, extraFunctions, newCoreFunctions, newExtraFunctions;
+
+
+    private MDDMapper mapper = null;
+    private int curTarget = 0;
+    private int nbBuffers = 0;
+
+    public MostPermissiveModifier(LogicalModel model) {
+        this.model = model;
+    }
+
+    @Override
+    public LogicalModel performTask() {
+        this.ddm = model.getMDDManager();
+        this.core = model.getComponents();
+        this.extra = model.getExtraComponents();
+        this.coreFunctions = model.getLogicalFunctions();
+        this.extraFunctions = model.getExtraLogicalFunctions();
+
+        return null;
+    }
+
+    @Override
+    public int get(int i) {
+        return i ; }
 }
 
 
